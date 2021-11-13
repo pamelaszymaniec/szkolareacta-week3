0e9800264996c8523c199a0de71b283da591d87f
Pamela Szymaniec <pamelaszymaniec@MacBook-Bajki-Pro.local>
2021-11-13 15:08:21 +0100

    bugfix - leaflet map

--- src/components/ApiUsersList/ApiUserLocation.js
+++ src/components/ApiUsersList/ApiUserLocation.js
@@ -1,16 +1,6 @@
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
 
-//fix showing Mark - https://stackoverflow.com/questions/49441600/react-leaflet-marker-files-not-found
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png').default,
-    iconUrl: require('leaflet/dist/images/marker-icon.png').default,
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png').default
-});
-    
 
 export function ApiUserLocation({ position }) {
     return (
